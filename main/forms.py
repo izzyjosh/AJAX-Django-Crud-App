1422769f3cf6c03966afdf85480abf14f620604e
izzyjosh <calebayuba222@gmail.com>
2023-11-18 19:37:29 +0100

    setup the front end part

---
+++ main/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from .models import PersonInfo
+
+
+class PersonInfoForm(forms.ModelForm):
+    first_name = forms.CharField(
+            label="",
+            required=True,
+            widget = forms.TextInput(attrs={
+                "name":"first_name",
+                "type":"text",
+                "class":"form-control",
+                "placeholder":"First Name",
+                }))
+
+
+    last_name = forms.CharField(
+            label="",                                                             required=True,
+            widget = forms.TextInput(attrs={
+                "name":"last_name",
+                "type":"text",
+                "class":"form-control",
+                "placeholder":"Last Name",
+                }))
+
+
+    department = forms.CharField(
+            label="",                                                             required=True,
+            widget = forms.TextInput(attrs={
+                "name":"department",
+                "type":"text",
+                "class":"form-control",
+                "placeholder":"Department",
+                }))
+
+
+    course = forms.CharField(
+            label="",                                                             required=True,
+            widget = forms.TextInput(attrs={
+                "name":"course",
+                "type":"text",
+                "class":"form-control",
+                "placeholder":"course",
+                }))
+
+    class Meta:
+        model = PersonInfo
+        fields = ("__all__")
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import PersonInfoForm
+from django.http import JsonResponse
 
-# Create your views here.
+def create(request):
+    if request.method == "POST":
+        form = PersonInfoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return JsonResponse({"status":"user saved successfully"})
+        else:
+            return JsonResponse({"status":"user not saved"})
+
+    else:
+        form = PersonInfoForm()
+
